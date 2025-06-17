82ccc2dee5ec2449c49c1bffbf3e4bdaff5492b2
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-18 00:31:12 +0530

    terraform

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block
+  instance_tenancy = "default"
+  enable_dns_hostnames = "true"
+
+  tags = merge(
+    local.common_tags,{
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id #association with VPC
+
+  tags = merge(
+    local.common_tags,{
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
