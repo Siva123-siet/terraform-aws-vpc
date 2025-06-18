60f9b0327656c4e407bc1eb3ea70c33648668cc1
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-18 11:54:52 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -4,4 +4,5 @@ locals {
     Environment = var.environment
     Terraform = "true"
   }
+  az_names = slice(data.aws_availability_zones.available.names,0,2)
 }
--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,5 @@
+#VPC roboshop-dev
+
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
@@ -10,6 +12,8 @@ resource "aws_vpc" "main" {
   )
 }
 
+#IGW roboshop-dev
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id #association with VPC
 
@@ -19,3 +23,19 @@ resource "aws_internet_gateway" "main" {
     }
   )
 }
+
+#roboshop-dev-us-east-1a
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.common_tags,{
+        Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    }
+  )
+} 
