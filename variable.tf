5cbb874fffcc2b443c0a1f2dca5180468f21652b
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-19 15:34:59 +0530

    terraform

--- variable.tf
+++ variable.tf
@@ -23,43 +23,61 @@ variable "database_subnet_cidrs" {
 }
 
 variable "vpc_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "igw_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "public_subnet_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "private_subnet_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "database_subnet_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "eip_tags" {
-  type = list(string)
+  type = map(string)
   default = {
   }
 }
 
 variable "nat_gateway_tags" {
-  type = list(string)
+  type = map(string)
+  default = {
+  }
+}
+
+variable "public_route_table_tags" {
+  type = map(string)
+  default = {
+  }
+}
+
+variable "private_route_table_tags" {
+  type = map(string)
+  default = {
+  }
+}
+
+variable "database_route_table_tags" {
+  type = map(string)
   default = {
   }
 }
